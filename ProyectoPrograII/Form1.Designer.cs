f744ed8e2747e1029252125215033b354c166e84
Lorena Gregorio Hernandez <lorenaghernandez@hotmail.com>
2019-03-06 18:52:21 -0600

    cambio

--- ProyectoPrograII/Form1.Designer.cs
+++ ProyectoPrograII/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.aerolineasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aerolineasToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
@@ -88,6 +89,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
