30aed929f7a143acc1e0e851d95f6e7b15fff1f8
Lorena Gregorio Hernandez <lorenaghernandez@hotmail.com>
2019-03-07 19:48:39 -0600

    cambops

--- ProyectoPrograII/AvionesForms.Designer.cs
+++ ProyectoPrograII/AvionesForms.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AvionesForms));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -238,6 +239,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(802, 502);
             this.Controls.Add(this.dataGridView1);
